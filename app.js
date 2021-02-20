657b4c8f8dc3c44605eb914709936f99e3af5106
alea <alea@aleaitsolutions.com>
2021-02-20 14:34:44 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Continuous Delivery Done'));
+app.get('/', (req, res) => res.send('Delivery and Pipeline Done'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
